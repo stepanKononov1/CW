af71aef00186418dee4c3d7424ec2d0fae3e5c0f
stepanKononov1 <94189109+stepanKononov1@users.noreply.github.com>
2023-10-15 23:45:21 +0300

    Add files via upload

--- CW2.pas
+++ CW2.pas
@@ -7,13 +7,13 @@ begin
     x:=i/10;
     if x<-7 then
       writeln(-Power(-x, 0.1*x)+x:5:2)
-    else if (x >= -7) and (x < 0) then
+    else if x < 0 then
       writeln('Ошибка: Х<0, действие lg(x) невыполнимо')
-    else if (x>=0) and (x<5) then
+    else if x < 5 then
       if x = 0 then
         writeln('Ошибка: Х=0, на ноль делить нельзя')
       else writeln(sin(x)/cos(x)/(-x):5:2)
-    else if x>=5 then
+    else if x >= 5 then
       writeln(sin(x)/cos(x)-x:5:2);
   end;
 end.
